f1f65bdd01cffeee3f21dd9b6ca56868724a8b39
Sarthak30 <>
2018-03-04 18:40:49 +0530

    Automate Backward elimination

--- regression/multiple_linear_regression/multiple_linear_regression.py
+++ regression/multiple_linear_regression/multiple_linear_regression.py
@@ -39,3 +39,45 @@ y_pred = regressor.predict(X_test)
 
 #Building a optimal model using Backward elimination
 import statsmodels.formula.api as sm
+X = np.append(arr=np.ones((50, 1)).astype(int), values=X, axis=1)
+
+def backward_elimination_with_p(y, x, sl):
+    num_vars = len(x[0])
+    for i in range(num_vars):
+        regressor_ols = sm.OLS(endog=y, exog=x).fit()
+        max_var =  max(regressor_ols.pvalues).astype(float)
+        if max_var > sl:
+            for j in range(0, num_vars-i):
+                if (regressor_ols.pvalues[j].astype(float) == max_var):
+                    x = np.delete(x, j, 1)
+    regressor_ols.summary()
+    return x
+
+def backward_elimination_with_p_and_adjR2(y, x, SL):
+    numVars = len(x[0])
+    temp = np.zeros((50,6)).astype(int)
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(y, x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        adjR_before = regressor_OLS.rsquared_adj.astype(float)
+        if maxVar > SL:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    temp[:,j] = x[:, j]
+                    x = np.delete(x, j, 1)
+                    tmp_regressor = sm.OLS(y, x).fit()
+                    adjR_after = tmp_regressor.rsquared_adj.astype(float)
+                    if (adjR_before >= adjR_after):
+                        x_rollback = np.hstack((x, temp[:,[0,j]]))
+                        x_rollback = np.delete(x_rollback, j, 1)
+                        print (regressor_OLS.summary())
+                        return x_rollback
+                    else:
+                        continue
+    regressor_OLS.summary()
+    return x
+    
+X_opt_with_only_p = backward_elimination_with_p(Y, X, 0.05)
+X_opt = backward_elimination_with_p_and_adjR2(Y, X, 0.05)
+
+
